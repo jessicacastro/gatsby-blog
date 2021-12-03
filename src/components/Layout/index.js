b837015ae99e9f88780e24cace3de41307cadf9d
Jessica Castro <jessica.castros@outlook.com>
2021-12-03 19:02:45 -0300

    [Refactor] Turns the layout into a component and changes its imports

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,6 +1,6 @@
 import * as React from "react"
 import PropTypes from "prop-types"
-import Profile from "./Profile"
+import Profile from "../Profile"
 
 const Layout = ({ children }) => {
   return (
